3a1368dfd4faedc4776e92061f2b0d90f2637bb0
Yoonjeong Ko <jejukyj@naver.com>
2024-09-09 01:39:44 +0900

    Docs : 컴포넌트 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import Header from './components/Header';
+import Content from './components/content';
+import InputBox from './components/InputBox';
+import GlobalStyle from './components/GlobalStyle';
+
+const wrapperStyle = css`
+  width: 100vw;
+  max-width: 430px;
+  height: 100vh;
+  background-image: linear-gradient(
+    to bottom right,
+    rgb(247, 196, 218),
+    rgb(239, 239, 239)
+  );
+`;
+
 function App() {
-  return <div>React SNS - test</div>;
+  return (
+    <div className="wrapper" css={wrapperStyle}>
+      <GlobalStyle />
+      <Header />
+      <Content />
+      <InputBox />
+    </div>
+  );
 }
 
 export default App;
