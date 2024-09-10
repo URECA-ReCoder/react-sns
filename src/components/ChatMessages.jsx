34b383798d3e7bb496a888d34de5ed3003616186
jissssu <113964353+jissssu@users.noreply.github.com>
2024-09-10 20:22:01 +0900

    feat: 메세 화면 표시 및 자동 스크롤 기능 추가

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 const MessagesContainer = styled.div`
@@ -41,19 +42,36 @@ const MessageText = styled.div`
   line-height: 1.4;
 `;
 
-export default function ChatMessages() {
+const ChatMessages = ({ messages, users }) => {
+  const containerRef = useRef(null); // 메시지 리스트 컨테이너를 참조하기 위한 useRef 훅
+
+  useEffect(() => {
+    // 메시지가 업데이트될 때 스크롤을 컨테이너의 가장 아래로 이동
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <MessagesContainer>
-      <Message>
-        <UserImage src="/path/to/default/image.png" alt="User" />
-        <MessageContent>
-          <MessageInfo>
-            <span>User Name</span>
-            <span>Time</span>
-          </MessageInfo>
-          <MessageText isUser={false}>Message content</MessageText>
-        </MessageContent>
-      </Message>
+    <MessagesContainer ref={containerRef}>
+      {messages.map((message) => {
+        const user = users[message.userId] || {}; // 메시지에 해당하는 사용자 정보
+        return (
+          <Message key={message.id} style={{ justifyContent: user.id === 0 ? 'flex-end' : 'flex-start' }}>
+            {user.id !== 0 && <UserImage src={user.userImage} alt={user.userName} />} 
+            <MessageContent>
+              <MessageInfo>
+                <span>{user.userName}</span> 
+                <span>{message.time}</span> 
+              </MessageInfo>
+              <MessageText isUser={user.id === 0}>{message.text}</MessageText> 
+            </MessageContent>
+            {user.id === 0 && <UserImage src={user.userImage} alt={user.userName} />} 
+          </Message>
+        );
+      })}
     </MessagesContainer>
   );
-}
+};
+
+export default ChatMessages;
