dcbeaa0c022d58d77d78a312c4dbdc0463eb31d7
Yoonjeong Ko <jejukyj@naver.com>
2024-09-11 01:20:40 +0900

    Design : width 수정, position 값 추가

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,18 +1,59 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import SubmitArrow from '../assets/submit.svg';
+import { useRef, useState } from 'react';
+
+function InputBox() {
+  const [text, setText] = useState('');
+  const inputRef = useRef(null);
+
+  function handleInputChange(e) {
+    setText(e.target.value);
+    // e.target에 있는 <input.../>으로부터 value 값을 가져옴
+  }
+
+  function onClickButton(){
+     // 공백 입력 방지
+     if (text.trim() === '') return;
+
+    setText('');
+    inputRef.current.focus(); // 버튼 누른 후에도 input box에 자동 포커싱
+  }
+
+  return (
+    <div css={wrapperStyle}>
+      <div css={inputBoxStyle}>
+        <form css={formStyle}>
+          <input
+            type="text"
+            placeholder="메시지를 입력해주세요."
+            css={inputStyle}
+            onChange={handleInputChange}
+            autoFocus
+          />
+          <img src={SubmitArrow} css={submitBtnStyle} onClick={onClickButton}/>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
+
+// emotion/css
 
 const wrapperStyle = css`
   border-top: 1px solid #1d1d1d;
   width: 100%;
+  max-width: 430px;
   height: 80px;
-  background-color: white;
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #f8ff2e;
-  position: absolute;
+  /* position: absolute; */
   bottom: 0;
+  position: fixed;
 `;
 
 const inputBoxStyle = css`
@@ -50,22 +91,3 @@ const submitBtnStyle = css`
   object-fit: contain;
   cursor: pointer;
 `;
-
-function InputBox() {
-  return (
-    <div css={wrapperStyle}>
-      <div css={inputBoxStyle}>
-        <form css={formStyle}>
-          <input
-            type="text"
-            placeholder="메시지를 입력해주세요."
-            css={inputStyle}
-          />
-          <img src={SubmitArrow} css={submitBtnStyle} />
-        </form>
-      </div>
-    </div>
-  );
-}
-
-export default InputBox;
