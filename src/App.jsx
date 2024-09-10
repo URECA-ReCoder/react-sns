5cbe1b9e262319c477ec0cebaab8b0d288473f61
Yoonjeong Ko <jejukyj@naver.com>
2024-09-11 01:14:28 +0900

    Style : width, height, position 단위 변경

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import InputBox from './components/InputBox';
 import GlobalStyle from './components/GlobalStyle';
 
 const wrapperStyle = css`
-  width: 100vw;
+  width: 100%;
   max-width: 430px;
-  height: 100vh;
+  height: 100%;
+  /* overflow-y: scroll; */
   background: linear-gradient(#f8ff2e, #ffffff 40%);
   border-left: 1px solid #1d1d1d;
   border-right: 1px solid #1d1d1d;
-  position: relative;
 `;
 
 function App() {
