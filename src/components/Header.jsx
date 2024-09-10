17a225a2458572a2647689719139a909733c5217
Yoonjeong Ko <jejukyj@naver.com>
2024-09-11 01:19:32 +0900

    Design : width 수정, position 값 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,41 @@ import Bell from '../assets/bell-ring.svg';
 import Camcoder from '../assets/video-camera.svg';
 import Profile from '../assets/minji.jpg';
 
+function Header() {
+  return (
+    <div css={headerStyle}>
+      <div css={leftIconStyle}>
+        <img src={Arrow} css={iconStyle} />
+        <div css={imgTextLayout}>
+          <img src={Profile} css={imgStyle} />
+          <div css={textStyle}>
+            <span css={nameStyle}>민지</span>
+            <span css={idStyle}>minji</span>
+          </div>
+        </div>
+      </div>
+      <div css={rightIconStyle}>
+        <img src={Bell} css={iconStyle} />
+        <img src={Camcoder} css={iconStyle} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
+
+// emotion/css
+
 const headerStyle = css`
-  border-bottom: 1px solid #1d1d1d;
+  width: 100%;
+  max-width: 430px;
+  background-color: #f9ff3c;
+  /* border-bottom: 1px solid #1d1d1d; */
   display: flex;
   justify-content: space-between;
   padding: 24px 28px 24px 28px;
+  position: fixed;
+  box-sizing: border-box;
 `;
 
 const iconStyle = css`
@@ -62,26 +92,3 @@ const idStyle = css`
   font-size: 14px;
   color: gray;
 `;
-
-function Header() {
-  return (
-    <div css={headerStyle}>
-      <div css={leftIconStyle}>
-        <img src={Arrow} css={iconStyle} />
-        <div css={imgTextLayout}>
-          <img src={Profile} css={imgStyle} />
-          <div css={textStyle}>
-            <span css={nameStyle}>민지</span>
-            <span css={idStyle}>minji</span>
-          </div>
-        </div>
-      </div>
-      <div css={rightIconStyle}>
-        <img src={Bell} css={iconStyle} />
-        <img src={Camcoder} css={iconStyle} />
-      </div>
-    </div>
-  );
-}
-
-export default Header;
