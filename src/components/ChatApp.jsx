71c2e7977b6b670daf8d1c59a52d9980825506aa
jissssu <113964353+jissssu@users.noreply.github.com>
2024-09-10 23:17:45 +0900

    fix: 메세지 상태 업데이트

--- src/components/ChatApp.jsx
+++ src/components/ChatApp.jsx
@@ -6,7 +6,6 @@ import ChatInput from './ChatInput';
 import chatData from '../public/ChatData.json'; 
 import userData from '../public/UserData.json'; 
 
-
 const ChatContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -25,24 +24,25 @@ const ChatApp = () => {
   useEffect(() => {
     // JSON 파일로부터 데이터를 가져와 상태 설정
     setMessages(chatData);
-   // 사용자 데이터를 객체 형태로 변환하여 상태에 설정
+    
+    // 사용자 데이터를 객체 형태로 변환하여 상태에 설정
     const usersMap = {};
     userData.forEach(user => {
       usersMap[user.id] = user;
     });
     setUsers(usersMap);
   }, []);
- // 새로운 메시지를 추가하는 함수
- const handleSendMessage = (newMessageText) => {
-  // 새로운 메시지 객체 생성
-  const newMessage = {
-    id: messages.length + 1, // 메시지 ID는 현재 메시지 길이 + 1로 설정
-    userId: 0, // 현재 사용자 ID (기본값은 0으로 설정)
-    time: new Date().toLocaleTimeString(), // 현재 시간을 메시지 시간으로 설정
-    text: newMessageText // 사용자가 입력한 메시지 텍스트
-  };
- // 새로운 메시지를 기존 메시지 목록에 추가하여 상태 업데이트
-    setMessages([...messages, newMessage]);
+
+  // 새로운 메시지를 추가하는 함수
+  const handleSendMessage = (newMessageText) => {
+    const newMessage = {
+      id: Date.now(), 
+      userId: 0,
+      time: new Date().toLocaleTimeString(), 
+      text: newMessageText 
+    };
+    // 상태 업데이트: 이전 상태를 기반으로 새로운 메시지 추가
+    setMessages(prevMessages => [...prevMessages, newMessage]);
   };
 
   return (
