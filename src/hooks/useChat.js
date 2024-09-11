493332103a6c88ccd763e7d3eb1802f68d239e15
hyorish03 <hyorish2001@gmail.com>
2024-09-11 22:05:45 +0900

    Feat: useChat 훅 구현

---
+++ src/hooks/useChat.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+import chat from '@assets/chat';
+function useChat() {
+  const [chats, setChats] = useState(chat);
+  function handleNewChat(chat) {
+    setChats((prevChats) => [...prevChats, chat]);
+  }
+  return { chats, handleNewChat };
+}
+
+export default useChat;
