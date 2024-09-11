96d95a752901fe777a9f2dda80347f112ac149fe
hyorish03 <hyorish2001@gmail.com>
2024-09-11 22:05:34 +0900

    Feat: 채팅 스타일링 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import Header from '@components/Header';
+import MessageInput from '@components/MessageInput';
+import PartnerMessage from '@components/ChatItem/PartnerMessage';
+import MyMessage from '@components/ChatItem/MyMessage';
+import { userId } from './constants';
+import useChat from '@hooks/useChat';
+
 function App() {
-  return <div>React SNS</div>;
+  const { chats } = useChat();
+  return (
+    <div>
+      <Header />
+      {chats.map((item, index) => {
+        return item.id === userId ? (
+          <MyMessage
+            id={item.id}
+            key={index}
+            nickname={item.nickname}
+            profileImage={item.image}
+            message={item.message}
+          />
+        ) : (
+          <PartnerMessage
+            key={index}
+            nickname={item.nickname}
+            profileImage={item.image}
+            message={item.message}
+          />
+        );
+      })}
+      <MessageInput />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ChatItem/styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const ChatItemWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: ${(props) =>
+    props.id === props.userId ? 'row-reverse' : 'row'};
+`;
+
+export const MyMessage = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  flex-direction: column;
+`;
+
+export const PartnerMessage = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-direction: column;
+  background-color: white;
+`;
+
+export const Chat = styled.div`
+  max-width: 80%;
+  min-height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 5px 20px;
+  cursor: pointer;
+  justify-content: ${(props) =>
+    props.id === props.userId ? 'flex-end' : 'flex-start'};
+  border-radius: ${(props) =>
+    props.id === props.userId ? '15px 1px 15px 15px' : ' 1px 15px 15px 15px'};
+  background-color: ${(props) =>
+    props.id === props.userId ? 'blueviolet' : '#f1f1f1'};
+  color: ${(props) => (props.id === props.userId ? 'white' : 'black')};
+  margin: 0 10px;
+`;
+
+export const ProfileImage = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
