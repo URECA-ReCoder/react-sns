b8d3b5ea52d83dbd85f748e9bbc71e3b58bf816e
hyorish03 <hyorish2001@gmail.com>
2024-09-11 20:40:31 +0900

    Feat: png 모듈화 파일 추가

--- src/assets/png/index.js
+++ src/assets/png/index.js
@@ -1,5 +1,6 @@
 import chevronLeft from './chevronLeft.png';
 import camera from './camera.png';
 import bell from './bell.png';
-
-export { chevronLeft, camera, bell };
+import find from './find.png';
+import send from './send.png';
+export { chevronLeft, camera, bell, find, send };
