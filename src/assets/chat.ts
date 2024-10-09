be740365afb8a6530d34ab24990b4a73f896cd7f
hyorish03 <hyorish2001@gmail.com>
2024-10-10 00:16:52 +0900

    fix: 채팅 정보  및 사용자 정보 업데이트

--- src/assets/chat.ts
+++ src/assets/chat.ts
@@ -1,52 +1,55 @@
-export const chat: ChatArray = [
-  {
-    userId: 1,
-    message: '안녕하세요',
-    timestamp: '2024-09-11T09:00:00Z',
-  },
-  {
-    userId: 0,
-    message: '네 안녕하세요',
-    timestamp: '2024-09-11T09:05:00Z',
-  },
-  {
-    userId: 1,
-    message: '오늘 저녁 뭐 드셨어요 ?',
-    timestamp: '2024-09-11T09:10:00Z',
-  },
-  {
-    userId: 0,
-    message: '네 저는 치킨을 먹었어요',
-    timestamp: '2024-09-11T09:15:00Z',
-  },
-  {
-    userId: 1,
-    message: '우와 무슨 치킨 드셨어요 ?',
-    timestamp: '2024-09-11T09:20:00Z',
-  },
-  {
-    userId: 0,
-    message: '사실 뻥이예요',
-    timestamp: '2024-09-11T09:25:00Z',
-  },
-  {
-    userId: 1,
-    message: '네?',
-    timestamp: '2024-09-11T09:30:00Z',
-  },
-  {
-    userId: 0,
-    message: '뻥이라구요',
-    timestamp: '2024-09-11T09:35:00Z',
-  },
-  {
-    userId: 1,
-    message: '이상한 사람이네요',
-    timestamp: '2024-09-11T09:40:00Z',
-  },
-  {
-    userId: 0,
-    message: 'ㅋㅋㅋ',
-    timestamp: '2024-09-11T09:45:00Z',
-  },
-];
+import userInfo from '@assets/userInfo.json';
+
+// 전체 채팅 애플리케이션 데이터
+export const chatAppData: {
+  users: UserInfo[];
+  chatRooms: ChatRoom[];
+} = {
+  users: userInfo,
+  chatRooms: [
+    {
+      roomId: 1,
+      roomName: '일반 채팅방',
+      messages: [
+        { userId: 1, message: '안녕하세요', timestamp: '2024-09-11T09:00:00Z' },
+        {
+          userId: 0,
+          message: '네 안녕하세요',
+          timestamp: '2024-09-11T09:05:00Z',
+        },
+        {
+          userId: 1,
+          message: '밥 드셨어요 ?',
+          timestamp: '2024-09-11T09:07:00Z',
+        },
+        // 다른 메시지들
+      ],
+    },
+    {
+      roomId: 2,
+      roomName: '저녁 메뉴 토론방',
+      messages: [
+        {
+          userId: 1,
+          message: '오늘 저녁 뭐 드셨어요 ?',
+          timestamp: '2024-09-11T09:10:00Z',
+        },
+        {
+          userId: 0,
+          message: '네 저는 치킨을 먹었어요',
+          timestamp: '2024-09-11T09:15:00Z',
+        },
+        {
+          userId: 1,
+          message: '무슨 치킨이요?',
+          timestamp: '2024-09-11T09:20:00Z',
+        },
+        {
+          userId: 0,
+          message: '양념 치킨이요',
+          timestamp: '2024-09-11T09:20:00Z',
+        },
+      ],
+    },
+  ],
+};
