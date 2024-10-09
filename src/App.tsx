86e7f34b7a5e22cb4e03c6c3f658f6180f6ebd57
Sieon <sieon.eve@gmail.com>
2024-10-10 02:29:23 +0900

    feat: retry

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,35 @@
-// src/App.tsx
-import React from 'react';
-import { Global, css } from '@emotion/react';
-import styled from '@emotion/styled';
-import ChatRoom from './Pages/ChatRoom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-// GlobalStyles 컴포넌트를 정의합니다
-const GlobalStyles: React.FC = () => (
-  <Global
-    styles={css`
-      @font-face {
-        font-family: 'Pretendard-Regular';
-        src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
-          format('woff');
-        font-weight: 400;
-        font-style: normal;
-      }
-      * {
-        box-sizing: border-box;
-        scrollbar-width: none;
-        font-family: 'Pretendard-Regular';
-      }
-      html {
-        margin: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-      }
-      body {
-        max-width: 430px;
-        width: 100%;
-        margin: 0;
-        height: 100%;
-        overflow: hidden;
-        :focus {
-          outline: none;
-        }
-      }
-      button {
-        border: none;
-        cursor: pointer;
-      }
-    `}
-  />
-);
+function App() {
+  const [count, setCount] = useState(0)
 
-// ContainerStyle 컴포넌트를 정의합니다
-const ContainerStyle = styled.div`
-  display: flex;
-  width: 100%;
-  max-width: 430px;
-  height: 100vh;
-  flex-direction: column;
-  background-color: #ffffff;
-  border: 1px solid #c9c9c9;
-`;
-
-// App 컴포넌트를 정의합니다
-const App: React.FC = () => {
   return (
     <>
-      <GlobalStyles />
-      <ContainerStyle>
-        <ChatRoom />
-      </ContainerStyle>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/compononts/form/Chatting.tsx
@@ -0,0 +1,73 @@
+import styled from '@emotion/styled';
+import IU from '../../public/images/IU.png';
+import Me from '../../public/images/Me.png';
+
+const ChattingContainer = styled.div`
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  gap: 20px;
+  overflow-y: auto;
+  margin-bottom: 70px;
+`;
+
+const SpeechBubble1 = styled.div`
+  width: fit-content;
+  max-width: 240px;
+  color: black;
+  display: flex;
+  padding: 10px;
+  border-radius: 2px 14px 14px 14px;
+  border: 1px solid #dfdfdf;
+  text-align: left;
+  align-items: flex-end;
+  white-space: pre-wrap;
+  font-weight: 400;
+  font-size: 14px;
+`;
+
+const SpeechBubble2 = styled.div`
+  background-color: #6245ff;
+  width: fit-content;
+  max-width: 240px;
+  color: #ffffff;
+  padding: 10px;
+  display: flex;
+  border-radius: 14px 2px 14px 14px;
+  text-align: left;
+  align-self: flex-end;
+  margin-left: auto;
+  white-space: pre-wrap;
+  font-size: 14px;
+`;
+
+const ProfileImg = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+`;
+
+const IdText = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  text-align: right;
+  margin: 0 5px 5px 0;
+  color: #979797;
+  font-size: 12px;
+  letter-spacing: -0.4px;
+`;
+
+const TimeStyle = styled.span`
+  display: flex;
+  justify-content: flex-end;
+  font-size: 10px;
+  color: #b6b6b6;
+  letter-spacing: -0.4px;
+`;
+
+const Chatting = 
+
+export default Chatting;
