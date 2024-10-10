43e819f71b58e88fcc08df1556a4ee3fe5ef26a0
Yoonjeong Ko <jejukyj@naver.com>
2024-10-10 11:31:03 +0900

    Feat : 자동 redirect 기능 추가

--- src/RouterList.tsx
+++ src/RouterList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import List from './List';
 import App from './App';
@@ -8,8 +8,10 @@ export default function RouterList() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Navigate to="/list" />} />
         <Route path="/list" element={<List />} />
         <Route path="/chatroom/:chatId" element={<App />} />
+        <Route path="*" element={<Navigate to="/list" />} />
       </Routes>
     </div>
   );
