d861007aeca3c7df6eaecb1e2f6b7a50dd882f2a
hyorish03 <hyorish2001@gmail.com>
2024-10-10 15:35:29 +0900

    fix: 라우팅 path 수정

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ export const HEADER_HEIGHT = 60;
 export const INPUT_HEIGHT = 60;
 
 export const ROUTE = {
-  CHATLIST: '/chatlist',
+  CHATLIST: '/',
   CHATINDIVIDUAL: '/chat/:id',
   NOTFOUND: '*',
 };
